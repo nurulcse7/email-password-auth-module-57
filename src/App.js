4c37be6a4e269de1b2d0231611bf5a81a5a5853b
nurulcse7 <nurul.cse7@gmail.com>
2022-10-16 14:45:04 +0600

    copy from ph

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import LoginBootsrap from './components/LoginBootsrap';
+import RegisterReactBootstrap from './components/RegisterReactBootstrap';
+import Main from './layout/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main>0</Main>,
+    children: [
+      {
+        path: '/',
+        element: <RegisterReactBootstrap></RegisterReactBootstrap>
+      },
+      {
+        path: '/register',
+        element: <RegisterReactBootstrap></RegisterReactBootstrap>
+      },
+      {
+        path: '/login',
+        element: <LoginBootsrap></LoginBootsrap>
+      },
+    ]
+  }
+])
+
+function App() {
+  return (
+    <div className="">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
